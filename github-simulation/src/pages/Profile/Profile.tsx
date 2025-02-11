fc70c4f1d504ec96becc6514465b457421f1d5f8
Samira <samira73talanchi@gmail.com>
2025-02-11 15:42:10 +0330

    fix userdataprofile type issue

--- github-simulation/src/pages/Profile/Profile.tsx
+++ github-simulation/src/pages/Profile/Profile.tsx
@@ -8,21 +8,30 @@ const Profile = () => {
 
     const [userProfileData, setUserProfileData] = useState<any>(null);
 
+
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         fetch("https://api.github.com/users/samiratalanchi")
-        .then((response) => {
-            if (!response.ok) {
-            throw new Error("Network response was not ok");
-            }
-            return response.json();
-        })
-        .then((data) => {
-            setUserProfileData(data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setUserProfileData(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+                setLoading(false);
+            });
     }, []);
+
+    if (loading) return <div className="text-center mt-10">Loading...</div>;
+    if (!userProfileData) return <div className="text-center mt-10">No user data found</div>;
+
 
     return (
         <HeaderLayout userProfileData={userProfileData}>
